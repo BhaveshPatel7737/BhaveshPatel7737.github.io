eb042a1705e4109772ffaa34982126ed5432b9ef
avp45 <arthvpatel77@gmail.com>
2024-03-03 22:18:20 -0500

    final commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let divcard = document.getElementById("card1");
+let front = document.getElementById("front");
+let back = document.getElementById("back");
+function myimg(){
+    front.style.display = "none";
+    back.style.display = "block";
+}
+
+function myinfo(){
+    front.style.display = "block";
+    back.style.display = "none";
+}
