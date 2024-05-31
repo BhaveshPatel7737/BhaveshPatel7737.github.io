ef482132ff13925c340ff8066c568f98b1dec991
BhaveshPatel7737 <bkpatel7737@gmail.com>
2024-05-31 01:24:12 -0400

    updated the projects

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let divcard = document.getElementById("card1");
 let front = document.getElementById("front");
 let back = document.getElementById("back");
-function myimg(){
+
+divcard.addEventListener("mouseover", function() {
     front.style.display = "none";
     back.style.display = "block";
-}
+});
 
-function myinfo(){
+divcard.addEventListener("mouseout", function() {
     front.style.display = "block";
     back.style.display = "none";
-}
+});
